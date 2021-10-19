aa0435a8668a5813aa96707772398ed480d4f0da
Michel Osee <michel.osee@ulb.be>
2021-10-19 16:00:08 +0200

    version du WE

--- greenVibes/greenVibes.ino
+++ greenVibes/greenVibes.ino
@@ -1,31 +1,60 @@
-#include <LiquidCrystal.h>
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
 #include "Wire.h"
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
 
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+
+#include <LiquidCrystal.h>
+
+#define LCD_RS  4
+#define LCD_EN  5
 #define LCD_D4  6
 #define LCD_D5  7
 #define LCD_D6  8
 #define LCD_D7  9
-#define LCD_EN  5
-#define LCD_RS  4
+
+#define TEST1   10
+#define TEST2   11
+#define TEST3   12
+
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
 
 #define TS        10    // sampling period in ms
-#define MEAN_SIZE 1000  // Number of samples for mean power calculation
+#define MEAN_SIZE 50   // Number of samples for mean power calculation
 #define KI        26    // gain of the current AFE in 1/ohm
 
 #define B   0.995
 #define A  -0.990
 
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-float voltage, current;
-unsigned long curTime,lastTime;
-int samplesNb;
-float power;
-bool newMpuSample;
-VectorInt16 acc;
-float period, lastCrossingTime;
-int lastSign;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 
 typedef enum {
@@ -34,33 +63,14 @@ typedef enum {
 } state_t;
 state_t state;
 
+float voltage=0, current=0;
+int samplesNb;
+float power = 0;
+float freq;
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 mpu;
-
-// MPU control/status vars
-bool     dmpReady = false;  // set true if DMP init was successful
-uint8_t  mpuIntStatus;      // holds actual interrupt status byte from MPU
-uint8_t  devStatus;         // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;        // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;         // count of all bytes currently in FIFO
-uint8_t  fifoBuffer[64];    // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion  q;          // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-// b0 and b1 are used by devStatus
-#define CONNECTION_ERROR  0x04
-#define OVERFLOW_ERROR    0x08
-uint8_t err = 0;
-uint8_t test[4] = {0,1,2,3};
-float testF = 1;
+float getFreq(int16_t acc);
+float readVoltage();
+float readCurrent();
 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
@@ -71,119 +81,215 @@ void dmpDataReady() {
     mpuInterrupt = true;
 }
 
-bool newSample;
 
-float readVoltage();
-float readCurrent();
-
-void mpuSetup();
-bool mpuAvailable();
-VectorInt16 mpuRead();
-
-
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
 
 void setup() {
-  mpuSetup();
+  // Timing test pins
+  pinMode(TEST1, OUTPUT);
+  pinMode(TEST2, OUTPUT);
+  pinMode(TEST3, OUTPUT);
+  
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+
+  // initialize serial communication
+  // (115200 chosen because it is required for Teapot Demo output, but it's
+  // really up to you depending on your project)
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  Serial.begin(115200);
-  samplesNb = 0;
-  power = 0;
-  newMpuSample = false;
+
+  // initialize device
+  lcd.clear();
+  lcd.print("Initialising MPU");
+  mpu.initialize();
+
+  // verify connection
+  lcd.clear();
+  if (!mpu.testConnection()) {
+    lcd.print("MPU init error");
+    while(1);
+  }
+
+  // load and configure the DMP
+  lcd.clear();
+  lcd.print("Initialising DMP");
+  devStatus = mpu.dmpInitialize();
+    
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    lcd.clear();
+    lcd.print("Enabling DMP");
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    dmpReady = true;
+    lcd.clear();
+    lcd.print("P =       mW");
+    lcd.setCursor(0, 1);
+    lcd.print("f =       Hz");
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    lcd.clear();
+    lcd.print("DMP error: ");
+    lcd.print(devStatus);
+    while(1);
+  }
   state = DISCONNECTED;
-  lastTime = millis();
-  lastCrossingTime = -1;
-  lastSign = 0;
-  period = 0;
 }
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
 
 void loop() {
   char c;
-  VectorInt16 acc;
   
-  if (Serial.available()) {
-    c = Serial.read();
-    if (c == 'S') {
-      samplesNb = 0;
-      power = 0;
-      lastTime = millis() - TS;
-      state = CONNECTED;
-    }
-    else if (c == 'E') {
-      Serial.println();
-      state = DISCONNECTED;
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize) {
+    if (Serial.available()) {
+      c = Serial.read();
+      if (c == 'S') {
+        samplesNb = 0;
+        power = 0;
+        state = CONNECTED;
+      }
+      else if (c == 'E') {
+        state = DISCONNECTED;
+      }
     }
   }
   
-  curTime = millis();
-  if (curTime >= lastTime + TS) {
+  digitalWrite(TEST1, HIGH);
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  digitalWrite(TEST3, HIGH);
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  digitalWrite(TEST3, LOW);
+  
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    lcd.clear();
+    lcd.print("FIFO reset");
+    while(1);
+  }
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+    // get real acceleration, adjusted to remove gravity
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+
+    freq = getFreq(aaReal.x);
     voltage = readVoltage();
     current = readCurrent();
-    lastTime += TS;
-    samplesNb++;
     power += voltage*current;
-    
+
+    samplesNb++;
     if (samplesNb >= MEAN_SIZE) {
+      digitalWrite(TEST2, HIGH);
       samplesNb = 0;
       power = (power*1000)/MEAN_SIZE;
       if (power < 0) {
         power = 0;
       }
-      lcd.clear();
-      lcd.print("P = ");
+      lcd.setCursor(4, 0);
       lcd.print(power);
-      lcd.print(" mW");
       power = 0;
-      lcd.setCursor(0, 1);
-      lcd.print("f = ");
-      lcd.print(1000/period);
-      lcd.print(" Hz");
+      lcd.setCursor(4, 1);
+      lcd.print(freq);
+      digitalWrite(TEST2, LOW);
     }
-  }
 
-
-  if (mpuAvailable()) {
-    acc = mpuRead();
-    newMpuSample = true;
-    if (lastSign == 0) {
-      if (acc.y < 0) {
-        lastSign = -1;
-      } else {
-        lastSign = 1;
-      }
-    } else {
-      if  ( (lastSign < 0) && (acc.y > 0) ) {
-        lastSign = 1;
-        if (lastCrossingTime < 0) {
-          lastCrossingTime = curTime;
-        }
-        else {
-          if ( (curTime - lastCrossingTime > 100) && (curTime - lastCrossingTime < 2000) ) {
-            period = curTime - lastCrossingTime;
-            lastCrossingTime = curTime;
-          }
-        }
-      }
-      else if ( (lastSign > 0) && (acc.y < 0) ) {
-        lastSign = -1;
-      }
+    if (state == DISCONNECTED) {
+      // nothing to do;
     }
-  }
-
-  if (state == DISCONNECTED) {
-    // nothing to do;
-  }
-  else if (state == CONNECTED) {
-    if (newMpuSample) {
-      newMpuSample = false;
+    else if (state == CONNECTED) {
       Serial.write(0x55);
-      Serial.write((byte*)&acc.x, 2);
-      Serial.write((byte*)&acc.y, 2);
-      Serial.write((byte*)&acc.z, 2);
+      Serial.write((byte*)&aaReal.x, 2);
+      Serial.write((byte*)&aaReal.y, 2);
+      Serial.write((byte*)&aaReal.z, 2);
       Serial.write((byte*)&voltage, 4);
       Serial.write((byte*)&current, 4);
     }
   }
+  digitalWrite(TEST1, LOW);
+}
+
+float getFreq(int16_t acc) {
+  static int16_t minAcc = 0, maxAcc = 0;
+  static uint32_t lastCrossingTime = 0;
+  static int16_t lastSign = 1;
+  static float freq = 0;
+  uint32_t curTime;
+  
+  curTime = millis();
+
+  if (acc > maxAcc) {
+    maxAcc = acc;
+  }
+  else if (acc < minAcc) {
+    minAcc = acc;
+  }
+    
+  if ( (lastSign < 0) && (acc > 0) ) {
+    lastSign = 1;
+    if (lastCrossingTime != 0) {
+      if ( (curTime - lastCrossingTime > 100) && (curTime - lastCrossingTime < 2000) && (maxAcc-minAcc > 1000) ) {
+        freq = 1000.0/(curTime - lastCrossingTime);
+      }
+      else {
+        freq = 0;
+      }
+      maxAcc = 0;
+      minAcc = 0;
+    }
+    lastCrossingTime = curTime;
+  }
+  else if ( (lastSign > 0) && (acc < 0) ) {
+    lastSign = -1;
+  }
+  return(freq);
 }
 
 
@@ -210,80 +316,4 @@ float readCurrent() {
   oldY = y;
 
   return(y);
-}
-
-
-void mpuSetup() {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  Wire.begin();
-  // initialize device
-  mpu.initialize();
-  // verify connection
-  if (!mpu.testConnection()) {
-    err = CONNECTION_ERROR;
-  }
-  // load and configure the DMP
-  devStatus = mpu.dmpInitialize();  
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // turn on the DMP, now that it's ready
-    mpu.setDMPEnabled(true);
-    // enable Arduino interrupt detection
-    attachInterrupt(0, dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    dmpReady = true;
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-      err |= devStatus;
-  }
-  newSample = false;
-}
-
-bool mpuAvailable() {
-  return(mpuInterrupt);
-}
-
-
-VectorInt16 mpuRead() {
-  // if there are no data to read
-  if (!mpuInterrupt) {
-    // put dummy values to fill the data packet
-    aaReal.x = 0;
-    aaReal.y = 0;
-    aaReal.z = 0;
-  } else {
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-    err = mpuIntStatus;
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-      // reset so we can continue cleanly
-      mpu.resetFIFO();
-      // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-      // wait for correct available data length, should be a VERY short wait
-      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-      // read a packet from FIFO
-      mpu.getFIFOBytes(fifoBuffer, packetSize);
-      // track FIFO count here in case there is > 1 packet available
-      // (this lets us immediately read more without waiting for an interrupt)
-      fifoCount -= packetSize;
-      // get real acceleration, adjusted to remove gravity
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetAccel(&aa, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    }
-  }
-  return(aaReal);
 }
--- greenVibesAcquisition.py
+++ greenVibesAcquisition.py
@@ -10,103 +10,104 @@ import os
 
 
 
+# Parametres de l'acquisition
+sampleFreq = 100
+
 # Configuration du port serie
 arduino = serial.Serial()    # creation de l'objet associe au port serie
 arduino.baudrate = 115200    # definition du baudrate
-arduino.port = 'COM25'        # selection du port serie
+arduino.port = 'COM6'        # selection du port serie
 arduino.timeout = 0.5
 
-
-# Parametres de l'acquisition
-sampleFreq = 100
-while True:
-    try:
-        sampleNb = sampleFreq*int(input("Entrez la durée de l'acquisition (en secondes) : "))
-        if sampleNb > 0:
-            break
-    except:
-        pass
-    print("La durée d'acquisition doit être un nombre entier strictement positif")
-
-print("synchro with the Arduino")
+print("Connection with the Arduino")
 # Connexion au dsPIC
 try:
     arduino.open()               # on tente d'etablir la connexion
 except:
-    print('Echec de la connexion')  # En cas de probleme,
-    sys.exit()                      # on ferme l'application
+    print('Connection failed')   # En cas de probleme,
+    sys.exit()                   # on ferme l'application
 data = 'e'
 while data != b'':
     data = arduino.read()
 print('Connected')
 time.sleep(2)
 
+active = True
+
+while active:
+    while True:
+        try:
+            sampleNb = sampleFreq*int(input("Entrez la durée de l'acquisition en secondes (0 pour sortir) : "))
+            if sampleNb >= 0:
+                break
+        except:
+            pass
+        print("La durée d'acquisition doit être un nombre entier strictement positif")
 
 
+    if sampleNb == 0:
+        active = False
+    else:
+        # Création du fichier de données
+        test = False
+        for folder in os.listdir():
+            if folder == 'data':
+                test = True
+                break;
+        if test == False:
+            os.mkdir('data')
 
-
-
-# Création du fichier de données
-test = False
-for folder in os.listdir():
-    if folder == 'data':
         test = True
-        break;
-if test == False:
-    os.mkdir('data')
-
-test = True
-a = 0
-while test:
-    a += 1
-    filename = "./data/experiment%i.csv" % a
-    test = os.access(filename, os.W_OK)
-
-labels = ["accX", "accY", "accZ", "E (V)", "I (A)"]
-csvFile = open(filename, mode='w', newline='')
-csvWriter = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-csvWriter.writerow(labels)
-
-print("Acquisition started")
-arduino.write(b'S')               # Envoi du caractere de synchro
-count = 0
-a = 0
-data = np.zeros((5, sampleNb))
-while a < sampleNb:
-    try:
-        tmp = unpack('B',arduino.read(1))[0]
-        if tmp == 0x55:
-            for b in range(3):
-                tmp = arduino.read(2)
-                tmp = unpack('h',tmp)       # convertit le 'Bytes' en l'entier correspondant (tmp devient un tuple de float)
-                data[b, a] = tmp[0]         # extrait le flottant du tuple (tmp devient un float)
-            for b in range(2):
-                tmp = arduino.read(4)
-                tmp = unpack('f',tmp)       # convertit le 'Bytes' en le flottant correspondant (tmp devient un tuple de float)
-                data[b+3, a] = tmp[0]       # extrait le flottant du tuple (tmp devient un float)
-            csvWriter.writerow(data[:,a])
+        a = 0
+        while test:
             a += 1
-        else:
-            print("Error: '0x55' expected, %s read" % hex(tmp))
-    except:
-        count += 1
-        if count > 10:
-            print('Acquisition aborted')
-            a = sampleNb
-arduino.write(b'E')               # Envoi du caractere de synchro
-csvFile.close()
-print("Acquisition finished")
+            filename = "./data/experiment%i.csv" % a
+            test = os.access(filename, os.W_OK)
+
+        labels = ["accX", "accY", "accZ", "E (V)", "I (A)"]
+        csvFile = open(filename, mode='w', newline='')
+        csvWriter = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvWriter.writerow(labels)
+
+        print("Acquisition started")
+        arduino.write(b'S')               # Envoi du caractere de synchro
+        count = 0
+        a = 0
+        data = np.zeros((5, sampleNb))
+        while a < sampleNb:
+            try:
+                tmp = unpack('B',arduino.read(1))[0]
+                if tmp == 0x55:
+                    for b in range(3):
+                        tmp = arduino.read(2)
+                        tmp = unpack('h',tmp)       # convertit les 'Bytes' en l'entier correspondant (tmp devient un tuple de float)
+                        data[b, a] = tmp[0]         # extrait le flottant du tuple (tmp devient un float)
+                    for b in range(2):
+                        tmp = arduino.read(4)
+                        tmp = unpack('f',tmp)       # convertit les 'Bytes' en le flottant correspondant (tmp devient un tuple de float)
+                        data[b+3, a] = tmp[0]       # extrait le flottant du tuple (tmp devient un float)
+                    csvWriter.writerow(data[:,a])
+                    a += 1
+                else:
+                    print("Error: '0x55' expected, %s read" % hex(tmp))
+            except:
+                count += 1
+                if count > 10:
+                    print('Acquisition aborted')
+                    a = sampleNb
+        arduino.write(b'E')               # Envoi du caractere de synchro
+        csvFile.close()
+        print("Acquisition finished")
+
+        t = np.arange(sampleNb)/sampleFreq
+        fig, ax = plt.subplots(5,1)
+        fig.suptitle(filename)
+        for a in range(5):
+            ax[a].plot(t, data[a])
+            ax[a].set_ylabel(labels[a])
+            ax[a].set_xlabel("t (s)")
+        plt.show()
 
 # Deconnexion du dsPIC
 arduino.close()
-
-t = np.arange(sampleNb)/sampleFreq
-fig, ax = plt.subplots(3,1)
-fig.suptitle(filename)
-for a in range(3):
-    ax[a].plot(t, data[a])
-    ax[a].set_ylabel(labels[a])
-    ax[a].set_xlabel("t (s)")
-plt.show()
-
 
